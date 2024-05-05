3d2908559f809f497d1720954bc4579a4abd64f3
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2024-05-05 17:51:55 +0200

    0501 struct init

--- 04_ownership/040301-slicing-strings/src/main.rs
+++ 04_ownership/040301-slicing-strings/src/main.rs
@@ -10,8 +10,7 @@ fn first_word(s: &String) -> &str {
     &s[..]
 }
 
-
-fn main() {
+fn slices_mutable_immutable() {
     let mut s: String = String::from("hello world");
 
     let word = first_word(&s);
@@ -25,4 +24,42 @@ fn main() {
     println!("s: {}", s);
     println!("the final character is: {}", &s[s.len()-1..]);
     // println!("{word}")  // cannot reference here as this would render the s.push_str() invalid
+}
+
+fn first_word_slice(s: &str) -> &str {
+
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn slices_as_parameters() {
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word_slice(&my_string[0..6]);
+    let word = first_word_slice(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word_slice(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word_slice(&my_string_literal[0..6]);
+    let word = first_word_slice(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word_slice(my_string_literal);
+}
+
+fn main() {
+    slices_mutable_immutable();
+    slices_as_parameters();
 }
---
+++ 05_structs/0501-struct-init/src/main.rs
@@ -0,0 +1,65 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn instantiating_directly() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    // the entire struct must be set as mutable
+    // to be able to change any of its fields
+    // user1.email = String::from("someoneelse@mail.com"):
+    println!("user1 email: {}", user1.email);
+
+    let mut user2 = User {
+        active: true,
+        username: String::from("anotherusername456"),
+        email: String::from("someoneaa@whatever.com"),
+        sign_in_count: 3,
+    };
+    user2.username = String::from("anotherusername789");
+    println!("user2 username: {}", user2.username);
+}
+
+fn init_functions() {
+    fn build_user(email: String, user: String) -> User {
+        User {
+            active: true,
+            username: user,
+            email, // field init shorthand, since the parameter and field have the same name
+            sign_in_count: 1,
+        }
+    }
+    build_user(String::from("what"), String::from("ever"));
+}
+
+fn struct_update_syntax() {
+    let user1 = User {
+        active: true,
+        username: String::from("whocares"),
+        email: String::from("someone@there.com"),
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        username: String::from("anotherusername"),
+        ..user1 // expand rest of fields
+
+    };
+
+    println!("user1: {}", user1.username);
+    println!("user2: {}", user2.username);
+}
+
+fn main() {
+    instantiating_directly();
+    init_functions();
+    struct_update_syntax();
+}
