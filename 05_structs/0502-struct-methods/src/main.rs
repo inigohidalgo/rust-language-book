abb48f1a0133a6e3caaa802d28c63def1f7fdd4e
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2024-06-02 14:26:02 +0200

    More methods

--- 05_structs/0502-struct-methods/src/main.rs
+++ 05_structs/0502-struct-methods/src/main.rs
@@ -8,6 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    /// Constructor which returns a rectangle with the same width and height
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -15,9 +26,16 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
     println!(
-        "The area of the rectangle is {} square pixels.",
+        "The area of the rectangle {:#?} is {} square pixels.",
+        rect1,
         rect1.area()
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
