d2c1615f6a6cae4eadde8398e1a21f361e0da46d
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2024-06-02 15:10:23 +0200

    catch-all pattern matching

--- 06_enums/0601-enum-init/src/main.rs
+++ 06_enums/0601-enum-init/src/main.rs
@@ -21,7 +21,28 @@ fn enum_attached_data() {
 }
 
 
+fn catch_all_patterns(){
+    fn add_fancy_hat() {println!("Adding hat")}
+    fn remove_fancy_hat() {println!("Removing hat")}
+    fn move_player(num_spaces: u8) {println!("Moving player {} spaces", num_spaces)}
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other), // this is a catch-all pattern which matches any other possible value dice_roll can take
+                                     // it binds the value to `other` which is then passed into `move_player`
+    }
+
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (), // _ is a special kwarg which does not bind to the matched value,
+                 //it is a way of expressing we want to ignore the value,
+                 // so rust will not raise an unused variable warning
+    }
+}
 
 fn main() {
     enum_attached_data();
+    catch_all_patterns();
 }
